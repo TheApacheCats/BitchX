e39eddbc0436523af1f914ef2b3feb42229bc710
Tim Cava <tcava@users.sourceforge.net>
2013-07-09 06:54:57 +0000

    Oops. Remove init_schat()'s forward declaration.
    git-svn-id: svn://svn.code.sf.net/p/bitchx/code/trunk@262 13b04d17-f746-0410-82c6-800466cd88b0

--- dll/arcfour/arcfour.h
+++ dll/arcfour/arcfour.h
@@ -11,5 +11,4 @@ static int send_dcc_encrypt (int, int, char *, int);
 static int get_dcc_encrypt (int, int, char *, int, int);
 static int start_dcc_crypt (int, int, unsigned long, int);
 static int end_dcc_crypt (int, unsigned long, int);
-static int init_schat(char *, char *, char *, char *, char *, char *, unsigned long, unsigned int);
 void dcc_sdcc (char *, char *);
