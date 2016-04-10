TheApacheCats/BitchX

commit 07cdd587b487b37f264b22e6b08923178c94cc10

Cleanup ircop_flags_to_str. Make the bufsize IRCD_BUFFER_SIZE instead of BIG_BUFFER_SIZE. Even with many flags enabled the output string shouldn't be larger than 512 bytes. Use strlcat rather than strmcat.
git-svn-id: svn://svn.code.sf.net/p/bitchx/code/trunk@407 13b04d17-f746-0410-82c6-800466cd88b0

source/notice.c
@@ -1050,15 +1050,15 @@ int i, j;
 
 char *ircop_flags_to_str(long flag)
 {
-int p, i;
-char *buffer = new_malloc(BIG_BUFFER_SIZE+1);
+	int i, p;
+	char *buffer = new_malloc(IRCD_BUFFER_SIZE);
 
 	for (i = 0, p = 1; opflags[i]; i++, p <<= 1)
 	{
 		if (flag & p)
 		{
-			strmcat(buffer, opflags[i], BIG_BUFFER_SIZE);
-			strmcat(buffer, ",", BIG_BUFFER_SIZE);
+			strlcat(buffer, opflags[i], sizeof buffer);
+			strlcat(buffer, ",", sizeof buffer);
 		}
 	}
 	if (*buffer)

commit 0b91d61496ff5af728a00cd86cacafee3b36896d

Fix conversion of SWATCH flags to string form
This was broken in commit [07cdd587], where 'sizeof' was used on a pointer
instead of the buffer pointed-to.

Fix this by having the caller of ircop_flags_to_str() supply a buffer and
length, since there's only two callers and both can use happily use stack
objects.

source/notice.c
@@ -1004,38 +1004,36 @@ int i, j;
 	return flag;
 }
 
-char *ircop_flags_to_str(long flag)
+char *ircop_flags_to_str(long flag, char *buffer, size_t len)
 {
-	int i, p;
-	char *buffer = new_malloc(IRCD_BUFFER_SIZE);
+	int i;
 
-	for (i = 0, p = 1; opflags[i]; i++, p <<= 1)
+	buffer[0] = 0;
+	for (i = 0; opflags[i]; i++)
 	{
-		if (flag & p)
+		if (flag & (1L << i))
 		{
-			strlcat(buffer, opflags[i], sizeof buffer);
-			strlcat(buffer, ",", sizeof buffer);
+			strlcat(buffer, opflags[i], len);
+			strlcat(buffer, ",", len);
 		}
 	}
-	if (*buffer)
-		chop(buffer, 1);
-	return buffer;
+	return chop(buffer, 1);
 }
 
 void print_ircop_flags(int server)
 {
-long flag;
-char *buffer = NULL;
-	flag = get_server_ircop_flags(server);
-	buffer = ircop_flags_to_str(flag);
+	char buffer[200];
+	long flag = get_server_ircop_flags(server);
+
+	ircop_flags_to_str(flag, buffer, sizeof buffer);
 	put_it("%s", convert_output_format("$G %bOper%BView%n: $0-", "%s", *buffer ? flag == -1 ? "ALL" : buffer : "NONE"));
-	new_free(&buffer);
 }
 
 void convert_swatch(Window *win, char *str, int unused)
 {
-unsigned long flag;
-char *p;
+	unsigned long flag;
+	char buffer[200];
+
 	if (from_server != -1)
 	{
 		flag = ircop_str_to_flags(get_server_ircop_flags(from_server), str);
@@ -1044,9 +1042,8 @@ char *p;
 	else
 		flag = ircop_str_to_flags(default_swatch, str);
 	default_swatch = flag;
-	p = ircop_flags_to_str(flag);
-	set_string_var(SWATCH_VAR, p);
-	new_free(&p);
+	ircop_flags_to_str(flag, buffer, sizeof buffer);
+	set_string_var(SWATCH_VAR, buffer);
 }
 
 void set_operview_flags(int server, unsigned long flags, int neg)